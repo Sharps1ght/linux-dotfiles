7b46d449470c0595a5f8fb7f029f4a4c347c3086
Sharps1ght <162005925+Sharps1ght@users.noreply.github.com>
2025-07-29 01:40:00 +0700

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -14,20 +14,29 @@ sleep 1
 echo "YOU called it upon yourself."
 sleep 3
 echo "Applying configs..."
+sleep 1
 cd .. && cp -rf $HOME/linux-dotfiles/* $HOME/.config
 echo "Updating package databases and upgrading installed packages..."
+sleep 1
 sudo pacman --noconfirm -Syyu
 echo "Installing the essentials..."
-sudo pacman --needed --noconfirm -S linux linux-headers linux-lts linux-lts-headers 7zip amd-ucode base base-devel btop fastfetch gtk2 gtk3 gtk4 hyprland hyprcursor hyprlang hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty neovim networkmanager noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn os-prober pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo swaync telegram-desktop wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one
-echo "Enabling necessary services (except ZeroTier One, it is not necessary)..."
-sudo systemctl enable --now pipewire
-sudo systemctl enable --now wireplumber
+sleep 1
+sudo pacman --needed --noconfirm -S linux linux-headers linux-lts linux-lts-headers 7zip btop fastfetch gtk2 gtk3 gtk4 hyprland hyprcursor hypridle hyprlang hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty neovim networkmanager noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo swaync telegram-desktop wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one
+echo "Enabling necessary services (except ZeroTier One, it is not necessary, but will still be activated)..."
+sleep 1
+systemctl --user enable --now pipewire
+systemctl --user enable --now wireplumber
 sudo systemctl enable --now zerotier-one
 echo "Installing yay..."
+sleep 1
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si && cd ..
 rm -rf yay
+echo "Installing a few pacakges from AUR using yay..."
+sleep 1
+yay --noconfirm -Syyu vesktop upscaler bottles
 echo "Installing OpenTabletDriver"
+sleep 1
 git clone https://aur.archlinux.org/opentabletdriver.git
 cd opentabletdriver && makepkg -si && cd ..
 rm -rf opentabletdriver
