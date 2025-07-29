f0202f0b4552bfb6e2637c231f1d1f2c55a3d582
Sharps1ght <162005925+Sharps1ght@users.noreply.github.com>
2025-07-29 10:01:29 +0700

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -15,13 +15,13 @@ echo "YOU called it upon yourself."
 sleep 3
 echo "Applying configs..."
 sleep 1
-cd .. && cp -rf $HOME/linux-dotfiles/* $HOME/.config
+cd $HOME && cp -rf $HOME/linux-dotfiles/* $HOME/.config
 echo "Updating package databases and upgrading installed packages..."
 sleep 1
 sudo pacman --noconfirm -Syyu
 echo "Installing the essentials..."
 sleep 1
-sudo pacman --needed --noconfirm -S ark thunar btop fastfetch gtk2 gtk3 gtk4 hyprland hyprcursor hypridle hyprlang hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty neovim networkmanager noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo swaync telegram-desktop uwsm vlc wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one noto-fonts noto-fonts-emoji nerd-fonts
+sudo pacman --needed --noconfirm -S ark thunar base-devel btop fastfetch gtk2 gtk3 gtk4 hyprland hyprcursor hypridle hyprlang hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty neovim networkmanager noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo swaync telegram-desktop uwsm vlc wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one noto-fonts noto-fonts-emoji nerd-fonts
 echo "Enabling necessary services (except ZeroTier One, it is not necessary, but will still be activated)..."
 sleep 1
 systemctl --user enable --now pipewire
@@ -29,16 +29,16 @@ systemctl --user enable --now wireplumber
 sudo systemctl enable --now zerotier-one
 echo "Installing yay..."
 sleep 1
-echo ""
 git clone https://aur.archlinux.org/yay.git
-cd yay && makepkg -si && cd ..
-rm -rf yay
+cd yay && makepkg -si
+cd $HOME
 echo "Installing a few pacakges from AUR using yay..."
 sleep 1
-yay --noconfirm -Syyu vesktop upscaler bottles
+yay --noconfirm -Syyu vesktop-bin upscaler bottles brave-bin
 echo "Installing OpenTabletDriver"
 sleep 1
 git clone https://aur.archlinux.org/opentabletdriver.git
-cd opentabletdriver && makepkg -si && cd ..
-rm -rf opentabletdriver
+cd opentabletdriver && makepkg -si
+cd $HOME
 sudo mkinitcpio -P && sudo rmmod wacom hid_uclogic
+rm -rf yay/ opentablerdriver/
