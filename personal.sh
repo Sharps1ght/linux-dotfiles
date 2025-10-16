73550aa5311eac4f229049af4c09bddb4548e25c
Sharps1ght <162005925+Sharps1ght@users.noreply.github.com>
2025-10-16 19:11:46 +0700

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -21,7 +21,7 @@ sleep 1
 sudo pacman --noconfirm -Syyu
 echo "Installing the essentials..."
 sleep 1
-sudo pacman --needed --noconfirm -S ark thunar base-devel btop dotnet-runtime fastfetch fish gtk2 gtk3 gtk4 hyprland hyprcursor hypridle hyprlang hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty mako neovim noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo telegram-desktop uwsm vlc wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one noto-fonts noto-fonts-emoji nerd-fonts
+sudo pacman --needed --noconfirm -S ark thunar base-devel btop dotnet-runtime fastfetch fish gtk2 gtk3 gtk4 hyprland hyprcursor hypridle hyprlang hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick jre8-openjdk jdk-openjdk kitty mako man-db neovim noto-fonts ntfs-3g nerd-fonts nvidia nvidia-utils openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi-wayland sudo telegram-desktop uwsm vlc wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one noto-fonts noto-fonts-emoji nerd-fonts
 sleep 1
 echo "Changing default shell to fish (cuz I personally prefer it)..."
 sleep 1
