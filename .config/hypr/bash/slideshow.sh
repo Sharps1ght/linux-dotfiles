cc79fbc7f3cb2e2a6d652020be4659d5041a1a04
Sharps1ght <oscarkambo3214@gmail.com>
2025-11-27 00:01:21 +0700

    update

--- .config/hypr/bash/slideshow.sh
+++ .config/hypr/bash/slideshow.sh
@@ -5,6 +5,7 @@ currentWall=$(hyprctl hyprpaper listactive)
 monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 timer=30
 
+sleep 1
 for display in $monitors; do
 	wallpaper=$(find "$wallpaperDir" -type f ! -name "$(basename "$(hyprctl hyprpaper listactive | grep $display)")" | shuf -n 1)
 	hyprctl hyprpaper reload "$display","$wallpaper"
