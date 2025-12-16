4620d9cf84a7f582e061825731e94e03c298db61
Sharps1ght <oscarkambo3214@gmail.com>
2025-12-16 17:29:13 +0700

    update

--- personal.sh
+++ personal.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-cp -r -v .config ..
 sudo pacman --needed --noconfirm -Syyu ark thunar btop chromium dnscrypt-proxy fastfetch fish gtk2 gtk3 gtk4 hyprland hyprlang hyprpicker hyprpolkitagent hyprshot hyprutils imagemagick kitty mako man-db man-pages neovim noto-fonts noto-fonts-emoji nerd-fonts nvidia-open openvpn pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse qt5-wayland qt6-wayland rofi swaybg vlc vlc-plugins-all wayland wireplumber wl-clipboard xdg-desktop-portal xdg-desktop-portal-hyprland zerotier-one
-chsh -s /usr/bin/fish $USER
 sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now dnscrypt-proxy.service
 systemctl --user enable --now pipewire
 systemctl --user enable --now wireplumber
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 sudo nvim /etc/dnscrypt-proxy/dnscrypt-proxy.toml
 sudo nvim /etc/resolv.conf
 sudo chattr +i /etc/resolv.conf
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo "If you want to reboot right now, press CTRL+C and use 'reboot'."
 for i in {1..30}; do
 	sleep 1
