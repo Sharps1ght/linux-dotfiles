c1b3c369f2e5cccb94a7d8432fa445027957a10e
Sharps1ght <oscarkambo3214@gmail.com>
2026-03-16 07:35:19 +0700

    broken now?

--- config-pull.sh
+++ config-pull.sh
@@ -2,10 +2,6 @@
 
 for element in $(find $(pwd)/.config -type f); do
 	target="$(echo $element | sed "s|^$(pwd)|$HOME|")"
-	if [[ $(stat -c%s $target) -ne $(stat -c%s $element) ]]; then
-		cp -r $target $element
-		echo "$target... done!"
-	else
-		continue
-	fi
+	cp -r $target $element
+	echo "$target... done!"
 done
